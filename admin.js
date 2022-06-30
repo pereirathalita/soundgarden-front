225a0ef1214bce1b619fa583f140fcf7eb7e9880
Thalita Pereira <pereirathalita@yahoo.com.br>
2022-06-30 19:44:47 -0300

    feat: responsividade admin

--- admin.js
+++ admin.js
@@ -11,11 +11,11 @@ function listarEventos(eventos) {
     const tr = document.createElement("tr");
     const attractionsStr = evento.attractions.toString();
     const data = new Date(evento.scheduled).toLocaleString().slice(0,-3);
-    tr.innerHTML = `<th scope="row">${index+4}</th>
-    <td>${data}</td>
-    <td>${evento.name}</td>
-    <td>${attractionsStr}</td>
-    <td>
+    tr.innerHTML = `<td scope="row" data-title="#">${index+4}</td>
+    <td data-title="Data">${data}xx</td>
+    <td data-title="Titulo">${evento.name}</td>
+    <td data-title="Atrações">${attractionsStr}</td>
+    <td class="no-xs-label">
         <a href="reservas.html?id=${evento._id}" class="btn btn-dark">ver reservas</a>
         <a href="editar-evento.html?id=${evento._id}" _id="${evento._id}" id="editar-evento" class="btn btn-secondary">editar</a>
         <a href="excluir-evento.html?id=${evento._id}" _id="${evento._id}" class="btn btn-danger">excluir</a>
